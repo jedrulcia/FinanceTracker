72b4dd3b30b5c50ad134c972924110fd95e3914c
Jedrulcia <jedrzej.koriat@gmail.com>
2025-01-23 20:41:41 +0100

    Add Scrollbar style

---
+++ FinanceTracker.WPF/ViewModels/MonthVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.WPF.ViewModels
+{
+    public class MonthVM
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public MonthVM(string name, int value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+    }
+}
